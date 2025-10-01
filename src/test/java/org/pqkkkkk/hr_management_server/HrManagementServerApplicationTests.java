6915ba9004f88504102223743254224c61e86c40
pqkkkk <22120174@student.hcmus.edu.vn>
2025-10-01 12:00:53 +0700

    Initialize Spring project

---
+++ src/test/java/org/pqkkkkk/hr_management_server/HrManagementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pqkkkkk.hr_management_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrManagementServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
