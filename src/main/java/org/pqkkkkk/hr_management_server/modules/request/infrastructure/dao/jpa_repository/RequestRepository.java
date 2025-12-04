b52f0b1c97dcb6084c5160f3c3ff5b8ad2fcc772
Budworld <giapnguyen0989@gmail.com>
2025-12-04 23:57:15 +0700

    Update RequestRepository.java de fix loi CI tren github

--- src/main/java/org/pqkkkkk/hr_management_server/modules/request/infrastructure/dao/jpa_repository/RequestRepository.java
+++ src/main/java/org/pqkkkkk/hr_management_server/modules/request/infrastructure/dao/jpa_repository/RequestRepository.java
@@ -1,9 +1,13 @@
 package org.pqkkkkk.hr_management_server.modules.request.infrastructure.dao.jpa_repository;
 
 import org.pqkkkkk.hr_management_server.modules.request.domain.entity.Request;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RequestRepository extends JpaRepository<Request, String> {
+    Page<Request> findAll(Specification<Request> spec, Pageable pageable);
 }
